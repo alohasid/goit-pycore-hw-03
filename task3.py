b60793beeba7cd80322414a150c60070acaeb30c
Oleksii Sydorenko <sandrez.spartanell@gmail.com>
2024-09-18 18:34:12 +0300

    completed task 3

---
+++ task3.py
@@ -0,0 +1,44 @@
+import re
+
+PHONE_CODE_NUMBER_BY_DEFAULT = '+38' # Can be changed to any country code
+PHONE_CODE_NUMBER_BY_DEFAULT_WITHOUT_PLUS = '38'
+
+def normalize_phone(phone_number: str) -> str:
+    # Remove all characters except digits and '+'
+    cleaned_phone_number = re.sub(r'[^\d+]', '', phone_number)
+    
+    # Check if the number starts with '+'
+    if cleaned_phone_number.startswith('+'):
+        # If it starts with '+38' or '+380', return it as is
+        if cleaned_phone_number.startswith(PHONE_CODE_NUMBER_BY_DEFAULT):
+            print('if 1')
+            return cleaned_phone_number
+        else:
+            # If it starts with '+' but not '+38', replace the country code
+            print('else 1')
+            return PHONE_CODE_NUMBER_BY_DEFAULT + cleaned_phone_number[1:]
+    elif cleaned_phone_number.startswith(PHONE_CODE_NUMBER_BY_DEFAULT_WITHOUT_PLUS):
+        return PHONE_CODE_NUMBER_BY_DEFAULT + cleaned_phone_number[2:]
+    else:
+        print('else 2')
+        # If it does not start with '+', prepend the default country code
+        return PHONE_CODE_NUMBER_BY_DEFAULT + cleaned_phone_number
+
+
+raw_numbers = [
+    "067\\t123 4567",
+    "(095) 234-5678\\n",
+    "+380 44 123 4567",
+    "380501234567",
+    "    +38(050)123-32-34",
+    "     0503451234",
+    "(050)8889900",
+    "38050-111-22-22",
+    "38050 111 22 11   ",
+]
+
+sanitized_numbers = [normalize_phone(num) for num in raw_numbers]
+print("Нормалізовані номери телефонів для SMS-розсилки:", sanitized_numbers)
+
+
+
